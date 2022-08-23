bdaaf6c6ef14ff84d242db210b5e61e4daaf81d7
root <Dhulkifli>
2022-08-23 08:06:41 -0700

    Pycodestyle - whitespace between parameters

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1,101):
+    for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
             num = 'FizzBuzz'
         elif num % 5 == 0:
