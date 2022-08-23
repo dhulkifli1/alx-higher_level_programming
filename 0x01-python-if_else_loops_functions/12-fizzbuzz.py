c3e7e00be792712c5ba3053d321189943bd5e184
root <Dhulkifli>
2022-08-23 08:02:27 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            num = 'FizzBuzz'
+        elif num % 5 == 0:
+            num = 'Buzz'
+        elif num % 3 == 0:
+            num = 'Fizz'
+        print(f"{num}", end='\n' if num == 100 else ' ')
