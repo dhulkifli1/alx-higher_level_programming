f033d0cc919ae845f7cbb5f9139c07256bf222ca
root <Dhulkifli>
2022-08-29 08:10:08 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence == "":
+        first = None
+    else:
+        first = sentence[0]
+    result = (length, first)
+    return result
