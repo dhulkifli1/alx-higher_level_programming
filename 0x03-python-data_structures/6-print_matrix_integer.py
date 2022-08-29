5cebcc052f2667fafd41c8572cac0f37e228bbc6
root <Dhulkifli>
2022-08-29 07:56:29 -0700

    6-print_matrix_integer.py - Cracked it

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in range(0, len(matrix)):
-        print("{}".format(matrix[x]))
+        for y in range(0, len(matrix)):
+            print("{}".format(matrix[x][y]), end = " " if y < len(matrix) - 1 else '\n')
