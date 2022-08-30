0fb42a2c4d88aa2b35954589a26e6275d52f6c3a
root <Dhulkifli>
2022-08-30 00:21:17 -0700

    9-max_integer.py - debugging

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
-    for int in my_list:
-        if int > max:
-            max = int
+    if my_list == []:
+        max = None
+    else:
+        max = my_list[0]
+        for int in my_list:
+            if int > max:
+                max = int
     return max
