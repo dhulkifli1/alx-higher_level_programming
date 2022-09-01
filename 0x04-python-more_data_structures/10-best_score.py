e2e088940adf19a919b9cf32a5ba92e5b39aa95b
root <Dhulkifli>
2022-09-01 04:55:32 -0700

    10-best_score.py - debugging

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     best_value = 0
-    if a_dictionary == None:
+    if a_dictionary == {}:
         return None
     else:
         for key, value in a_dictionary.items():
@@ -9,5 +9,5 @@ def best_score(a_dictionary):
                 best_value = value
             else:
                 continue
-        best_key = {i for i in a_dictionary if a_dictionary[i] == best_value}
-        return best_key
+        best_key = list({i for i in a_dictionary if a_dictionary[i] == best_value})
+        return best_key[0]
