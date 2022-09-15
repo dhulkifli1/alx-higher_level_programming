eee69534a58344caac99df289631af089e2e0c5d
root <Dhulkifli>
2022-09-14 23:02:45 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define a Square"""
+
+
+class Square:
+    """Defines a Square class"""
+
+    def __init__(self, size=0):
+        """Ensures size is an int and is greater than 0
+
+        Parameters:
+            size (int): size of the square
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for self.__size private variable"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for self.__size private variable"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns current square area"""
+
+        return self.__size * self.__size
