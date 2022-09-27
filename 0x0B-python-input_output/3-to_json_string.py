339a99d0296150e23d4390b4c80de3778b21e31f
root <Dhulkifli>
2022-09-27 06:02:32 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a JSON representation function"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object"""
+
+    return json.dumps(my_obj)
