31a8c9f9359a78ceced2bd09b24a2160c90152f1
root <Dhulkifli>
2022-09-27 07:22:52 -0700

    incorrect encoding

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation"""
 
-    with open(filename, 'w', encoding = 'UTP-8') as myFile:
+    with open(filename, 'w', encoding = 'UTF-8') as myFile:
         return json.dumps(my_obj, myFile)
