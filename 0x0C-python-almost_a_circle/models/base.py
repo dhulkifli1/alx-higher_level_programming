783a63ee87f04ec853ca08b32f783159aec7cdde
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-09-30 05:52:59 -0700

    Created base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains a Base class"""
+
+
+class Base:
+    """Represents a Base class"""
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Class constructor"""
+
+        if id != None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = id
