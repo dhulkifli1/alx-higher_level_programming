cc0f2b672806284efee560b6f74fb46d0b32d1d5
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-10-03 05:56:22 -0700

    Added getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,16 @@ class Square(Rectangle):
 
         return f"[Square] ({self.id}) {self.x}/{self.y}" \
                f" - {self.width}"
+
+    @property
+    def size(self):
+        """Getter for size"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+
+        self.width = value
+        self.height = value
