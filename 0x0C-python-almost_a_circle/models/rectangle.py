c59b0f740357f0f97bc6f1131a30a9b6ceac5f94
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-10-02 23:14:43 -0700

    Added str special method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,80 +15,86 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """Getter for width"""
+    def __str__(self):
+        """String method for Rectangle class"""
 
-            return self.__width
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y}"
+                f" - {self.width}/{self.height}")
 
-        @width.setter
-        def width(self, value):
-            """Setter for width"""
+    @property
+    def width(self):
+        """Getter for width"""
 
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+        return self.__width
 
-        @property
-        def height(self):
-            """Getter for height"""
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
 
-            return self.__height
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @height.setter
-        def height(self, value):
-            """Setter for height"""
+    @property
+    def height(self):
+        """Getter for height"""
 
-            if type(value) != int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        return self.__height
 
-        @property
-        def x(self):
-            """Getter for x"""
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
 
-            return self.__x
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @x.setter
-        def x(self, value):
-            """Setter for x"""
+    @property
+    def x(self):
+        """Getter for x"""
 
-            if type(value) != int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+        return self.__x
 
-        @property
-        def y(self):
-            """Getter for y"""
+    @x.setter
+    def x(self, value):
+        """Setter for x"""
 
-            return self.__y
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @y.setter
-        def y(self, value):
-            """Setter for y"""
+    @property
+    def y(self):
+        """Getter for y"""
 
-            if type(value) != int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y"""
+
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
 
-        def area(self):
-            """Returns the area value of the Rectangle instance"""
+    def area(self):
+        """Returns the area value of the Rectangle instance"""
 
-            return self.width * self.height
+        return self.width * self.height
 
-        def display(self):
-            """Prints in stdout the Rectangle with the character #"""
+    def display(self):
+        """Prints in stdout the Rectangle with the character #"""
 
-            for height in range(self.height):
-                for width in range(self.width):
-                    print("#", end="")
-                print("")
+        for height in range(self.height):
+            for width in range(self.width):
+                print("#", end="")
+            print("")
