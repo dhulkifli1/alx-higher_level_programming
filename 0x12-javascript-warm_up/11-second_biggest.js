09da4336eb047252f5e04dd9a68aab435edaf2c5
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-11-28 22:08:39 +0300

    Fixed one value argument bug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
 function secondBiggest (myList) {
-  let myListLength = myList.length;
-  if (myListLength === 0 || myListLength === 1) {
-    console.log('0');
-  } else {
     const myListSet = new Set(myList);
     myList = Array.from(myListSet);
-    myListLength = myList.length;
-    const sndLastIndex = myListLength - 2;
-    myList = myList.sort();
-    console.log(myList[sndLastIndex]);
+    let myListLength = myList.length;
+    if (myListLength === 0 || myListLength === 1) {
+      console.log('0');
+    } else {
+      const sndLastIndex = myListLength - 2;
+      myList = myList.sort();
+      console.log(myList[sndLastIndex]);
+    }
   }
-}
 
 secondBiggest(process.argv.slice(2));
