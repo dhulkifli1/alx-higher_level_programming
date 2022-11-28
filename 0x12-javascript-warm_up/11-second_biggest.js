c26233bdf88cde92410041ad725a59e1d6e4883b
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-11-28 22:10:15 +0300

    semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 function secondBiggest (myList) {
-    const myListSet = new Set(myList);
-    myList = Array.from(myListSet);
-    let myListLength = myList.length;
-    if (myListLength === 0 || myListLength === 1) {
-      console.log('0');
-    } else {
-      const sndLastIndex = myListLength - 2;
-      myList = myList.sort();
-      console.log(myList[sndLastIndex]);
-    }
+  const myListSet = new Set(myList);
+  myList = Array.from(myListSet);
+  const myListLength = myList.length;
+  if (myListLength === 0 || myListLength === 1) {
+    console.log('0');
+  } else {
+    const sndLastIndex = myListLength - 2;
+    myList = myList.sort();
+    console.log(myList[sndLastIndex]);
   }
+}
 
 secondBiggest(process.argv.slice(2));
