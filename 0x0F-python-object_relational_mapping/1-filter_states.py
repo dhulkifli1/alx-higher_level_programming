687d495c74e70960a6db0bb2a7b3c245d68bb3e0
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-12-22 10:26:42 +0300

    Task 1 modified

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Lists all states from the database hbtn_0e_0_usa'''
+'''Lists states starting by N from the database hbtn_0e_0_usa'''
 
 import MySQLdb
 import sys
@@ -9,9 +9,10 @@ if __name__ == '__main__':
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3], charset='utf8')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id ASC;')
+    cur.execute('SELECT * FROM states ORDER BY id ASC;')
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
     cur.close()
     conn.close()
