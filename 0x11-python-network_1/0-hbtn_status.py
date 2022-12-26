e8190bb41d284f4a77b566e535b0815cbe284a14
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-12-26 14:58:22 +0300

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Script that fetches https://alx-intranet.hbtn.io/status'''
+
+
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode()}")
