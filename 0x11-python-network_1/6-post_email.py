ed117ff53bca09584ab3bf8c5412ecb5d54fb653
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-12-26 18:12:25 +0300

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+'''
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    emails = {'email': sys.argv[2]}
+    response = requests.post(url, params=emails)
+    print(response.text)
