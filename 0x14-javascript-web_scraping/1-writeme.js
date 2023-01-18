cee2737ebf9f7ad12e1eb0fbb872a852c12c2a21
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-18 11:47:45 +0300

    Fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const sentence = process.argv[3];
 
-fs.writeFile(filePath, sentence, 'utf-8', (err, data) => {
+fs.writeFile(filePath, sentence, 'utf-8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
