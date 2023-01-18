f4a4af059f8ba2115845633b729c1c05baa9e1a6
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-18 15:49:24 +0300

    Semistandard fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(`error: ${error}`);
+  } else {
     console.log(`code: ${response.statusCode}`);
+  }
 });
