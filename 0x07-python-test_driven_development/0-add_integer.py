e9c63bb0e1c81d34fe2cc12d90c4dd743db3d883
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-03-09 17:39:21 +0300

    Task 0 done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains function add_integer"""
+
+def add_integer(a, b=98):
+    """Adds 2 integers"""
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    else:
+        if type(a) == "float":
+            a = int(a)
+        elif type(b) == "float":
+            b = int(b)
+        else:
+            return int(a + b)
